aa45aa4a04c70ac3a7323b2c0bf04f562f6d91a6
Victor Johnson <johnsonsirv@gmail.com>
2019-06-06 14:45:58 +0200

    my_each_with_index method to return index of enum

--- lib/enumerable.rb
+++ lib/enumerable.rb
@@ -1,7 +1,12 @@
 module Enumerable
   def my_each
+    raise TypeError.new("can't iterate range input") if self.is_a?(Range) && !self.begin.respond_to?(:succ)
     return self.enum_for(:my_each) unless block_given?
     0.upto(self.size - 1) { |i| yield(self.to_a[i]) }
     self
+  end
+
+  def my_each_with_index
+    
   end
 end
--- spec/enumerable_methods_spec.rb
+++ spec/enumerable_methods_spec.rb
@@ -5,8 +5,10 @@ describe Enumerable do
   let(:input_array) { [-6,-4,9,1] }
   let(:range_values) { (-2..2) }
   let(:unsupported_range_values) { (1.5..5) }
-  let(:proc_block) { proc { input_array.my_each |elem| elem.negative? } }
- describe '#my_each' do
+  let(:proc_block) { proc { |elem| elem.positive? } }
+  # let(:block_with_result) { proc { |elem| result << elem if elem.negative? } }
+
+  describe '#my_each' do
    context 'no block is given' do
      it 'should return an enumerable' do
        expect(input_array.my_each).to be_a(Enumerator)
@@ -14,21 +16,18 @@ describe Enumerable do
    end
    context 'when block is given' do
     it 'should call block once for each element in an array, passing element as paramater' do
-        input_array.my_each{ |e| result << e if e.positive? }
-        expect(result).to eql([9,1])
+      input_array.my_each{ |elem| result << elem if elem.negative? }
+      expect(result).to eql([-6,-4])
     end
-    it 'should call block once for each element in an array, passing element as paramater' do
-      range_values.my_each{ |e| result << e if e.negative? }
+    it 'should call block once for each sequence in range, passing element as paramater' do
+      range_values.my_each{ |elem| result << elem if elem.negative? }
       expect(result).to eql([-2,-1])
-  end
+    end
     it 'should return array when called on array' do
       expect(input_array.my_each{ proc_block }).to eql(input_array)
     end
     it 'should return range when called on range' do
       expect(range_values.my_each{ proc_block }).to eql(range_values)
-    end
-    it 'should raise TypeError if begin object of range does not support succ' do
-      expect(unsupported_range_values.my_each{ proc_block }).to eql(range_values)
     end
    end
  end
