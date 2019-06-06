cc8cdaf5cccf74f8d67d04325ab88fcff414a054
Victor Johnson <johnsonsirv@gmail.com>
2019-06-06 12:52:19 +0200

    my_each return obj and convert to_a internal for range

--- lib/enumerable.rb
+++ lib/enumerable.rb
@@ -1,6 +1,7 @@
 module Enumerable
   def my_each
     return self.enum_for(:my_each) unless block_given?
+    0.upto(self.size - 1) { |i| yield(self.to_a[i]) }
     self
   end
 end
--- spec/enumerable_methods_spec.rb
+++ spec/enumerable_methods_spec.rb
@@ -1,17 +1,35 @@
 require './lib/enumerable'
 
 describe Enumerable do
-  let(:input_array) { [6,4,9,1] }
+  let(:result) { [] }
+  let(:input_array) { [-6,-4,9,1] }
+  let(:range_values) { (-2..2) }
+  let(:unsupported_range_values) { (1.5..5) }
+  let(:proc_block) { proc { input_array.my_each |elem| elem.negative? } }
  describe '#my_each' do
    context 'no block is given' do
      it 'should return an enumerable' do
        expect(input_array.my_each).to be_a(Enumerator)
      end
    end
-   context 'an array is given' do
-     it 'should return an array' do
-       
-     end
+   context 'when block is given' do
+    it 'should call block once for each element in an array, passing element as paramater' do
+        input_array.my_each{ |e| result << e if e.positive? }
+        expect(result).to eql([9,1])
+    end
+    it 'should call block once for each element in an array, passing element as paramater' do
+      range_values.my_each{ |e| result << e if e.negative? }
+      expect(result).to eql([-2,-1])
+  end
+    it 'should return array when called on array' do
+      expect(input_array.my_each{ proc_block }).to eql(input_array)
+    end
+    it 'should return range when called on range' do
+      expect(range_values.my_each{ proc_block }).to eql(range_values)
+    end
+    it 'should raise TypeError if begin object of range does not support succ' do
+      expect(unsupported_range_values.my_each{ proc_block }).to eql(range_values)
+    end
    end
  end
 end
